d6ea69408975476313fb9b8aab0d95463123a683
AndrewTomMet <andij.tomchyshyn@stfalcon.com>
2017-01-18 18:08:26 +0200

    Fix for sensioLabsInsight number2

---
+++ tests/ClientBundle/Entity/CategoryEntityTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests\ClientBundle\Entity;
+
+use ClientBundle\Entity\Category;
+
+/**
+ * Class CategoryEntityTest
+ */
+class CategoryEntityTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testAdd()
+    {
+        $category = new Category();
+    }
+}
