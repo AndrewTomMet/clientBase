67edb2b85be2e0d7c0a4fe46beda7bbcabb11797
AndrewTomMet <andij.tomchyshyn@stfalcon.com>
2017-01-19 09:56:55 +0200

    Add test category

--- tests/ClientBundle/Entity/CategoryEntityTest.php
+++ tests/ClientBundle/Entity/CategoryEntityTest.php
@@ -3,6 +3,7 @@
 namespace tests\ClientBundle\Entity;
 
 use ClientBundle\Entity\Category;
+use ClientBundle\Entity\Client;
 
 /**
  * Class CategoryEntityTest
@@ -12,8 +13,27 @@ class CategoryEntityTest extends \PHPUnit_Framework_TestCase
     /**
      *
      */
-    public function testAdd()
+    public function testCount()
     {
-        $category = new Category();
+        $client = new Client();
+        $client->setFirstname('Dev');
+
+        $category0 = new Category();
+        $category0->setName('Test0');
+
+        $category1 = new Category();
+        $category1->setName('Test1');
+
+        $category0->addClient($client);
+        $category1->addClient($client);
+        $client->addCategory($category0);
+        $client->addCategory($category1);
+        $this->assertCount(1, $category0->getClients(), 'test category '.$category0->getName().' clients count');
+        $this->assertCount(2, $client->getCategories(), 'test client '.$client->getFirstname().' category count');
+
+        $category0->removeClient($client);
+        $client->removeCategory($category0);
+        $this->assertCount(0, $category0->getClients(), 'test category '.$category0->getName().' clients count');
+        $this->assertCount(1, $client->getCategories(), 'test client '.$client->getFirstname().' category count');
     }
 }
