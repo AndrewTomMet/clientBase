7552a362c55cf0f84ca7be854bd8f37a09d576e6
AndrewTomMet <andij.tomchyshyn@stfalcon.com>
2017-01-27 17:14:38 +0200

    Delete declered but never used

--- src/ClientBundle/Entity/Client.php
+++ src/ClientBundle/Entity/Client.php
@@ -5,7 +5,6 @@ namespace ClientBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-use ClientBundle\Validator\Constraints as ClientAssert;
 
 /**
  * Class Client
